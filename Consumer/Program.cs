8e117a14dffd9afbef54be237a2adee53a9ba1cf
george.emad <george.emad9819@gmail.com>
2024-06-10 21:59:28 +0300

    create consumer project

---
+++ Consumer/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using RabbitMQ.Client;
+
+
+var connectionFactory = new ConnectionFactory(
+    Uri = new Uri("amqp:guest:quest@localhost:5672"),
+    ClientProvidedName = "Producer App" 
+)
