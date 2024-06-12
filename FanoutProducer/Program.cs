8d63f1d39a640e35a2b1f4b1fa3b9fadbfc71b22
george.emad <george.emad9819@gmail.com>
2024-06-12 22:25:32 +0300

    add fanout implementation producer

--- FanoutProducer/Program.cs
+++ FanoutProducer/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using System.Collections;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Producer Fanout"
+};
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchange: "FanOutExchange", type: ExchangeType.Fanout);
+
+var message = "This message is published to all in pub/sub pattern";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("FanOutExchange", "", null, encodedMessage);
+
+Console.WriteLine(message);
