13628368e16719b4a8d4b0ee8437fc36a6b4d258
george.emad <george.emad9819@gmail.com>
2024-06-16 01:42:20 +0300

    Add implementation for Request Reply Pattern

--- Request_Reply_Pattern/Server/Program.cs
+++ Request_Reply_Pattern/Server/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Server"
+};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "request-queue", exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, EventArgs) =>
+{
+    var message = EventArgs.Body.ToArray();
+    var decodedMessage = Encoding.UTF8.GetString(message);
+    Console.WriteLine($"Recieved RRequest: {decodedMessage}");
+    var properties = channel.CreateBasicProperties();
+    properties.CorrelationId = EventArgs.BasicProperties.CorrelationId;
+
+    var replyMessage = "Reply message";
+    var encodedMessage = Encoding.UTF8.GetBytes(replyMessage);
+    channel.BasicPublish("", routingKey: EventArgs.BasicProperties.ReplyTo, basicProperties:properties, encodedMessage);
+    Console.WriteLine($"Reply Sent: {replyMessage}");
+
+};
+channel.BasicConsume(queue:"request-queue", autoAck: false,  consumer);
+
+
+Console.WriteLine("Enter to Exit");
+Console.ReadLine();
+
+
